650f4cd7dc1d6ab3141111e9e21c8fb2f3f76289
Alex Corzo <alexcorzo@gmail.com>
2015-04-17 09:46:26 -0500

    Parse email basics

--- app/tasks/MailerTask.php
+++ app/tasks/MailerTask.php
@@ -11,10 +11,13 @@ namespace Tasks;
 
 use \Cli\Output as Output;
 
+use \SoulboxCron\Models\Repositories\SbMessage as RepoMessage,
+
 //http://stackoverflow.com/questions/6004453/how-to-remove-multiple-deleted-files-in-git-repository
 class MailerTask extends \Phalcon\Cli\Task {
 
 	public function processAction() {
+        
         echo "\ncron to process!\n\n";
 	}
 }
---
+++ private/test_lib.php
@@ -0,0 +1,33 @@
+<?php
+    require_once('../app/library/utilities/mail/MimeMailParser.class.php');
+
+    $path = '../sandbox/maildump/1428945227_312.mail';
+    $Parser = new MimeMailParser();
+    $Parser->setPath($path);
+
+    $to = $Parser->getHeader('to');
+    $from = $Parser->getHeader('from');
+    $subject = $Parser->getHeader('subject');
+    $text = $Parser->getMessageBody('text');
+    $html = $Parser->getMessageBody('html');
+    $attachments = $Parser->getAttachments();
+
+    echo("\n\nto = $to<br />");
+    echo("\n\nfrom = $from<br />");
+    echo("\n\nsubject = $subject<br />");
+    echo("\n\ntext = $text<br />");
+    echo("\n\nhtml = $html<br />");
+    //echo("\n\nattachments = $attachments<br />");
+
+    $save_dir = '../sandbox/mailattachments/';
+    foreach($attachments as $attachment) {
+      // get the attachment name
+      $filename = $attachment->filename;
+      // write the file to the directory you want to save it in
+      if ($fp = fopen($save_dir.$filename, 'w')) {
+        while($bytes = $attachment->read()) {
+          fwrite($fp, $bytes);
+        }
+        fclose($fp);
+      }
+    }    
